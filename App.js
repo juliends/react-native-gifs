0e90019a746caeaeb8b7bb4efbcc3f4d22e7dadd
julien da silva <julien.da.silva@hotmail.com>
2018-01-02 19:37:23 +0100

    done_for_today

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, Image, ScrollView, Button, Alert, TouchableHighlight,  } from 'react-native';
 
-const styles = StyleSheet.create({
+const styles = {
+  main: {
+    flex: 1
+  },
   container: {
-    paddingTop: 20,
+    alignItems: 'center',
+    paddingTop: 25,
     paddingBottom: 10,
     backgroundColor: '#fff',
   },
   list: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap'
   },
   gif: {
-    width: 100,
-    height: 100
-  },
-  mainGif: {
-    width: 200,
-    height: 200
+    width: 120,
+    height: 120
   },
   input: {
     height: 50
   }
-});
+};
 
 const giphy = require('giphy-api')('fwyZq0pVmzjsbbqSNwuc51Y86Ved2ATP'); // API KEY
 
@@ -28,7 +32,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gifIds: ['xT9IgDEI1iZyb2wqo8', 'xT9IgDEI1iZyb2wqo8'],
+      gifIds: [],
       selectedGif: 'xT9IgDEI1iZyb2wqo8'
     };
   }
@@ -42,18 +46,16 @@ export default class App extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Gif giphyId={this.state.selectedGif} style={styles.mainGif} />
-        <SearchBar handleChange={this.searchGif} />
-        <ScrollView>
-          <GifList style={styles.list} gifIds={this.state.gifIds} handleSelectedGif={this.handleSelectedGif} />
-        </ScrollView>
-        <Button
-          onPress={() => {
-            Alert.alert('You tapped the button!');
-          }}
-          title="Press Me"
-        />
+      <View style={styles.main}>
+        <View style={styles.container}>
+          <Gif giphyId={this.state.selectedGif} />
+          <SearchBar handleChange={this.searchGif} />
+        </View>
+        <View>
+          <ScrollView>
+            <GifList gifIds={this.state.gifIds} handleSelectedGif={this.handleSelectedGif} />
+          </ScrollView>
+        </View>
       </View>
     );
   }
@@ -65,7 +67,7 @@ class Gif extends Component {
       uri: `https://media.giphy.com/media/${this.props.giphyId}/giphy.gif`
     };
     return (
-      <TouchableHighlight 
+      <TouchableHighlight
           onPress={() => {
             this.props.handleSelectedGif(this.props.giphyId);
           }}>
@@ -77,7 +79,7 @@ class Gif extends Component {
 
 const GifList = (props) => {
   return (
-    <View>
+    <View style={styles.list}>
       {
         props.gifIds.map(id => <Gif key={id} giphyId={id} handleSelectedGif={props.handleSelectedGif} />)
       }
